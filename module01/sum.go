81d29ff4654a593fc6dcdf780396f55fa352308b
Muhammad Yusril Yuriis <61834564+myyuriis@users.noreply.github.com>
2021-02-25 00:29:32 +0700

    Update sum.go

--- module01/sum.go
+++ module01/sum.go
@@ -2,15 +2,34 @@ package module01
 
 // Sum will sum up all of the numbers passed
 // in and return the result
+
+// Solved using normal way
+//func Sum(numbers []int) int {
+//	total := 0
+//	if len(numbers) == 0 {
+//		return 0
+//	}
+//
+//	for _, number := range numbers {
+//		total = total + number
+//	}
+//
+//	return total
+//}
+
+// Solved using recursive function
 func Sum(numbers []int) int {
-	total := 0
+	// This is the base case
 	if len(numbers) == 0 {
 		return 0
 	}
 
-	for _, number := range numbers {
-		total = total + number
-	}
-
-	return total
+	return numbers[0] + Sum(numbers[1:])
 }
+
+//Explanation for the recursive function
+//Sum(3, 4, 5) =>
+//	3 + Sum(4, 5) =>
+//		4 + Sum(5) =>
+//			5 + Sum() =>
+//				0 (This is the base case)
