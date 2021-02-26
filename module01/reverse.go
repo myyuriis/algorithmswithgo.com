42ee3387f214ccdb9452906e4ceb86350882f120
Muhammad Yusril Yuriis <61834564+myyuriis@users.noreply.github.com>
2021-02-26 19:49:40 +0700

    update reverse

--- module01/reverse.go
+++ module01/reverse.go
@@ -1,22 +1,58 @@
 package module01
 
-import (
-	"strings"
-)
-
 // Reverse will return the provided word in reverse
 // order. Eg:
 //
 //   Reverse("cat") => "tac"
 //   Reverse("alphabet") => "tebahpla"
 //
+// Using strings.Split function (reverse for loop)
+//func Reverse(word string) string {
+//	reversed := ""
+//	splitted := strings.Split(word, "")
+//
+//	for i := len(splitted) - 1; i >= 0; i-- {
+//		reversed = reversed + splitted[i]
+//	}
+//	return reversed
+//}
+
+// Using string() function to convert byte to string (reverse for loop)
+//func Reverse(word string) string {
+//	reversed := ""
+//
+//	for i := len(word)-1; i >= 0; i-- {
+//		reversed = reversed + string(word[i])
+//	}
+//	return reversed
+//}
+
+// Using string builder
+//func Reverse(word string) string {
+//	var sb strings.Builder
+//
+//	for i := len(word)-1; i >= 0; i-- {
+//		sb.WriteByte(word[i])
+//	}
+//	return sb.String()
+//}
+
+// Using normal order (not reversed for loop)
+//func Reverse(word string) string {
+//	rev := ""
+//
+//	for i := 0; i < len(word); i++ {
+//		rev = string(word[i]) + rev
+//	}
+//	return rev
+//}
+
+// Using for range to account for multi-byte characters (test: 日本語)
 func Reverse(word string) string {
-	reversed := ""
-	splitted := strings.Split(word, "")
+	rev := ""
 
-	for i := len(splitted) - 1; i >= 0; i-- {
-		reversed = reversed + splitted[i]
+	for _, v := range word {
+		rev = string(v) + rev
 	}
-
-	return reversed
+	return rev
 }
