86c2f66adcef88a6c8f30d09fa38ed4cdfbbe39b
Muhammad Yusril Yuriis <61834564+myyuriis@users.noreply.github.com>
2021-02-26 19:22:16 +0700

    [solved] reverse

--- module01/reverse.go
+++ module01/reverse.go
@@ -1,5 +1,9 @@
 package module01
 
+import (
+	"strings"
+)
+
 // Reverse will return the provided word in reverse
 // order. Eg:
 //
@@ -7,5 +11,12 @@ package module01
 //   Reverse("alphabet") => "tebahpla"
 //
 func Reverse(word string) string {
-	return ""
+	reversed := ""
+	splitted := strings.Split(word, "")
+
+	for i := len(splitted) - 1; i >= 0; i-- {
+		reversed = reversed + splitted[i]
+	}
+
+	return reversed
 }
